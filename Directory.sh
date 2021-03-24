07fac2a9ac33af46e7e183eeaa88acc9d8eb3961
alfonsoCourse <78757534+alfonsoCourse@users.noreply.github.com>
2021-03-24 15:48:20 -0400

    Add files via upload

---
+++ Directory.sh
@@ -0,0 +1,5 @@
+#ask the user and make it 
+echo "what is the name of the directory you want to check"
+read dir
+mkdir $dir
+#as default linux will tell the user if the directory already exist
---
+++ calculator.bat
@@ -0,0 +1,5 @@
+@echo off
+set/p calculation= your calculation:
+set/a calculation0=%calculation%
+echo %calculation%=%calculation0%
+pause
