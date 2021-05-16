5d4313133341d98f4eae150c1422d12240cd080f
alfonsoCourse <78757534+alfonsoCourse@users.noreply.github.com>
2021-05-16 20:49:14 +0000

    Add files via upload

---
+++ python/pythonMaths.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#This is Maths
+print(50+50) #add
+print(50/2) #Divide
+print(50-50) #Substract
+print(50*50) #Multiply
+print(50**50)#Exponent
+print(50+50-50*50/50) #Order operations
+print(50%6) #modulo, left over number
+print(50//6) #No leftover
+
---
+++ python/pythonNotes.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+print('Hello World')#Print a string
+print('\n')#NewLine
+print('''T
+h
+i
+s ''')#MultiLine
+
+
