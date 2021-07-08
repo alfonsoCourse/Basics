36ea023a2c7fc578b0a81924406f26f49c7ffa39
alfonsoCourse <78757534+alfonsoCourse@users.noreply.github.com>
2021-07-08 18:44:54 +0200

    Add files via upload

---
+++ NetLoop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Popular un archivo de txt para hacer de base de lista de IPs
+#Modificar los intervalos de cada ping ej. un ping cada 3s 
+#Cuantos pings
+echo "stop after replies "
+read inter
+echo " How many seconds between sending each packet?"
+read count
+echo " Create a new file every x seconds"
+read time
+
+while true; do 
+now=$(date)
+
+#Hello.txt is a list of IP add. you can add more IPs while the loop is working
+
+file=$(cat ./"IPs".txt)
+
+for i in $file
+do
+ping -i $inter -c $count $i >> "$i""_""$now".txt &
+done
+
+sleep $time
+done
