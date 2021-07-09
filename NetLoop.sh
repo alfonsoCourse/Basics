76870348c06fc55b2fb0528fb77b9737483e8483
alfonsoCourse <78757534+alfonsoCourse@users.noreply.github.com>
2021-07-09 12:28:46 +0200

    Update NetLoop.sh

--- NetLoop.sh
+++ NetLoop.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-echo " Cuantos pings por archivo?"
-	read count
-echo " Cada cuantos segundos quieres que se mande cada PING?"
-	read inter
-ideal=$(( $count * $inter))
-echo "Tiempo de cada nuevo archivo. Se recomienda: $ideal segundos"
-	read time
-echo " Cuantos archivos desea ?"
-	read filesnum
-total=$(($filesnum*$time))
-echo " Tiempo estimado: $total segundos, equivalente a "$((total/60))" minutos"
+#echo " Cuantos pings por archivo?"
+#	read count
+#echo " Cada cuantos segundos quieres que se mande cada PING?"
+#	read inter
+#ideal=$(( $count * $inter))
+#echo "Tiempo de cada nuevo archivo. Se recomienda: $ideal segundos"
+#	read time
+#echo " Cuantos archivos desea ?"
+#	read filesnum
+#total=$(($filesnum*$time))
+#echo " Tiempo estimado: $total segundos, equivalente a "$((total/60))" minutos"
 f=1
-while [ $f -le $filesnum ] 
+while [ $f -le $3 ] 
 do 
 	now=$(date "+%Y_%m_%d_%H_%M_%S")
 	#IPs.txt is a list of IP add. you can add more IPs while the loop is working
 	file=$(cat ./"IPs".txt)
 		for i in $file
 			do
-				ping -i $inter -c $count $i >> "$i""-""$now".txt &
-				echo "$i""-""$now""------>No.$f out of $filesnum"
+				ping -i 1 -c $2 $i >> "$1""-""$i""-""$now".txt &
+				echo "$1""$i""-""$now""------>No.$f out of $filesnum"
 			done
 	((f++))
-	sleep $time
+	sleep $2
 done
+
+#Dispositivo Pings   No.archivos
